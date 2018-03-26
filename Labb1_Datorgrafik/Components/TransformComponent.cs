3f68f613a3e8f17dce4f1d3c56eebe8595b8be2b
Katthatt <Jakobsson670@hotmail.com>
2018-03-26 15:56:10 +0200

    System Templates + TransformComponent done

---
+++ Labb1_Datorgrafik/Components/TransformComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1_Datorgrafik.Components
+{
+    class TransformComponent : IComponent
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+        public Vector3 Scale { get; set; }
+    }
+}
---
+++ Labb1_Datorgrafik/Systems/HeightMapSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Labb1_Datorgrafik.Systems
+{
+    class HeightMapSystem : ISystem
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gametime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
