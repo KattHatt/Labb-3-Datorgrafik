ef9b9eed6eb43b9ec8c9935c92caf29c801100a9
slayme89 <rob.gustavsson@hotmail.com>
2018-03-27 10:22:19 +0200

    changed camera component

--- Labb1_Datorgrafik/Components/CameraComponent.cs
+++ Labb1_Datorgrafik/Components/CameraComponent.cs
@@ -5,7 +5,6 @@ namespace Labb1_Datorgrafik
 {
     public class CameraComponent : IComponent
     {
-        public Vector3 Position { get; set; }
         public Vector3 Target { get; set; }
         public Matrix ProjectionMatrix { get; set; }
         public Matrix ViewMatrix { get; set; }
@@ -13,18 +12,8 @@ namespace Labb1_Datorgrafik
         public CameraComponent(GraphicsDevice gd)
         {
             Target = new Vector3(0f, 0f, 0f);
-            Position = new Vector3(0f, 0f, -100f);
-            ProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
-                MathHelper.ToRadians(45f),
-                gd.DisplayMode.AspectRatio,
-                1f,
-                1000f
-                );
-            ViewMatrix = Matrix.CreateLookAt(
-                Position,
-                Target,
-                new Vector3(0f, 1f, 0f)
-                );
+            ProjectionMatrix = new Matrix();
+            ViewMatrix = new Matrix();
         }
     }
 }
