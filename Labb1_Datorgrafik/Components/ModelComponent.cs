ab4dc358c337b4f74501969f9b23895eab8757c4
slayme89 <rob.gustavsson@hotmail.com>
2018-03-27 11:00:04 +0200

    Added modelComponent

---
+++ Labb1_Datorgrafik/Components/ModelComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Labb1_Datorgrafik.Components
+{
+    public class ModelComponent : IComponent
+    {
+        public Model Model { get; set; }
+
+        public ModelComponent(Model model)
+        {
+            Model = model;
+        }
+    }
+}
