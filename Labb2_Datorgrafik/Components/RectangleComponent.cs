7c20069ab0b7df53fbf638fc1f48eec895795907
slayme89 <rob.gustavsson@hotmail.com>
2018-04-12 21:29:35 +0200

    Added stuff to rectangle comp+system

--- Labb2_Datorgrafik/Components/RectangleComponent.cs
+++ Labb2_Datorgrafik/Components/RectangleComponent.cs
@@ -6,6 +6,9 @@ namespace Labb1_Datorgrafik.Components
 {
     public class RectangleComponent : IComponent
     {
+        // Graphics device
+        public GraphicsDevice graphicsDevice;
+
         // Rectangle vertex positions
         public Vector3 FRONT_TOP_LEFT;
         public Vector3 FRONT_TOP_RIGHT;
@@ -19,7 +22,33 @@ namespace Labb1_Datorgrafik.Components
         // Vertices
         public VertexPositionNormalTexture[] vertices;
 
-        // Rectangle Normals
+        // Vetrex buffer
+        public VertexBuffer vertexBuffer;
+
+        // Indices
+        public int[] indices;
+
+        // Index buffer
+        public IndexBuffer indexBuffer;
+
+        // Textures
+        public Texture2D frontFace;
+        public Texture2D leftFace;
+        public Texture2D rightFace;
+        public Texture2D topFace;
+        public Texture2D botFace;
+        public Texture2D backFace;
+
+        // Texture paths
+        public string FrontTexturePath { set; get; }
+        public string LeftTexturePath { set; get; }
+        public string RightTexturePath { set; get; }
+        public string TopTexturePath { set; get; }
+        public string BotTexturePath { set; get; }
+        public string BackTexturePath { set; get; }
+
+
+        // Rectangle normals
         public Vector3 RIGHT = new Vector3(1, 0, 0); // +X
         public Vector3 LEFT = new Vector3(-1, 0, 0); // -X
         public Vector3 UP = new Vector3(0, 1, 0); // +Y
@@ -27,10 +56,7 @@ namespace Labb1_Datorgrafik.Components
         public Vector3 FORWARD = new Vector3(0, 0, 1); // +Z
         public Vector3 BACKWARD = new Vector3(0, 0, -1); // -Z
 
-
-
-
-
+        // Cube with no texture
         public RectangleComponent()
         {
             FRONT_TOP_LEFT = new Vector3(-0.5f, 0.5f, 0.5f);
@@ -43,8 +69,28 @@ namespace Labb1_Datorgrafik.Components
             BACK_BOTTOM_RIGHT = new Vector3(0.5f, -0.5f, -0.5f);
         }
 
-        public RectangleComponent(float height, float width, float depth)
+        // Custom Rectangle with texture
+        public RectangleComponent(
+            GraphicsDevice gd,
+            float height,
+            float width,
+            float depth,
+            string frontTP,
+            string rightTP,
+            string leftTP,
+            string topTP,
+            string botTP,
+            string backTP)
         {
+            graphicsDevice = gd;
+
+            FrontTexturePath = frontTP;
+            LeftTexturePath = leftTP;
+            RightTexturePath = rightTP;
+            TopTexturePath = topTP;
+            BotTexturePath = botTP;
+            BackTexturePath = backTP;
+
             FRONT_TOP_LEFT = new Vector3(-width / 2, height / 2, depth / 2);
             FRONT_TOP_RIGHT = new Vector3(width / 2, height / 2, depth / 2);
             FRONT_BOTTOM_LEFT = new Vector3(-width / 2, -height / 2, depth / 2);
@@ -54,5 +100,7 @@ namespace Labb1_Datorgrafik.Components
             BACK_BOTTOM_LEFT = new Vector3(-width / 2, -height / 2, -depth / 2);
             BACK_BOTTOM_RIGHT = new Vector3(width / 2, -height / 2, -depth / 2);
         }
+
+       
     }
 }
---
+++ Labb2_Datorgrafik/Systems/RectangleSystem.cs
@@ -0,0 +1,121 @@
+﻿using Labb1_Datorgrafik.Components;
+using Labb2_Datorgrafik.Managers;
+using Labb2_Datorgrafik.Systems;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace Labb1_Datorgrafik.Systems
+{
+    public class RectangleSystem : ISystem, IRender
+    {
+        public void Load(ContentManager content)
+        {
+            ComponentManager cm = ComponentManager.GetInstance();
+
+            foreach(var entity in cm.GetComponentsOfType<RectangleComponent>())
+            {
+                RectangleComponent rect = (RectangleComponent)entity.Value;
+
+                SetupVertices(rect);
+                rect.frontFace = content.Load<Texture2D>(rect.FrontTexturePath);
+                rect.rightFace = content.Load<Texture2D>(rect.RightTexturePath);
+                rect.leftFace = content.Load<Texture2D>(rect.LeftTexturePath);
+                rect.topFace = content.Load<Texture2D>(rect.TopTexturePath);
+                rect.botFace = content.Load<Texture2D>(rect.BotTexturePath);
+                rect.backFace = content.Load<Texture2D>(rect.BackTexturePath);
+
+                SetupIndices(rect);
+                SetupIndexBuffer(rect);
+
+
+            }
+        }
+
+        public void Render(GraphicsDevice gd, BasicEffect be)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gametime)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Fill rectangle vertex list
+        private void SetupVertices(RectangleComponent r)
+        {
+            List<VertexPositionNormalTexture> vertexList = new List<VertexPositionNormalTexture>(36);
+
+            // Front face
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_LEFT, r.FORWARD, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_RIGHT, r.FORWARD, new Vector2(1, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_LEFT, r.FORWARD, new Vector2(0, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_LEFT, r.FORWARD, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_RIGHT, r.FORWARD, new Vector2(1, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_RIGHT, r.FORWARD, new Vector2(1, 0)));
+
+            // Top face
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_LEFT, r.UP, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_RIGHT, r.UP, new Vector2(1, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_LEFT, r.UP, new Vector2(0, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_LEFT, r.UP, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_RIGHT, r.UP, new Vector2(1, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_RIGHT, r.UP, new Vector2(1, 0)));
+
+            // Right face
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_RIGHT, r.RIGHT, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_RIGHT, r.RIGHT, new Vector2(1, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_RIGHT, r.RIGHT, new Vector2(0, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_RIGHT, r.RIGHT, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_RIGHT, r.RIGHT, new Vector2(1, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_RIGHT, r.RIGHT, new Vector2(1, 0)));
+
+            // Bottom face
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_LEFT, r.DOWN, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_RIGHT, r.DOWN, new Vector2(1, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_LEFT, r.DOWN, new Vector2(0, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_LEFT, r.DOWN, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_RIGHT, r.DOWN, new Vector2(1, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_RIGHT, r.DOWN, new Vector2(1, 0)));
+
+            // Left face
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_LEFT, r.LEFT, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_LEFT, r.LEFT, new Vector2(1, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_LEFT, r.LEFT, new Vector2(0, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_LEFT, r.LEFT, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_TOP_LEFT, r.LEFT, new Vector2(1, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.FRONT_BOTTOM_LEFT, r.LEFT, new Vector2(1, 0)));
+
+            // Back face
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_RIGHT, r.BACKWARD, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_LEFT, r.BACKWARD, new Vector2(1, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_RIGHT, r.BACKWARD, new Vector2(0, 0)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_RIGHT, r.BACKWARD, new Vector2(0, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_TOP_LEFT, r.BACKWARD, new Vector2(1, 1)));
+            vertexList.Add(new VertexPositionNormalTexture(r.BACK_BOTTOM_LEFT, r.BACKWARD, new Vector2(1, 0)));
+
+            r.vertices = vertexList.ToArray();
+        }
+
+        // Fills a rectangles index list
+        private void SetupIndices(RectangleComponent r)
+        {
+            List<int> indexList = new List<int>(36);
+
+            for (int i = 0; i < 36; ++i)
+                indexList.Add(i);
+
+            r.indices = indexList.ToArray();
+        }
+
+        // Set Index buffer
+        private void SetupIndexBuffer(RectangleComponent r)
+        {
+            r.indexBuffer = new IndexBuffer(r.graphicsDevice, typeof(short), r.indices.Length, BufferUsage.None);
+            r.indexBuffer.SetData(r.indices);
+        }
+    }
+}
