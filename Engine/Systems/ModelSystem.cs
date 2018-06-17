b39fffa339de8e6617d44c23419f65a8ff152b29
slayme89 <rob.gustavsson@hotmail.com>
2018-06-17 09:44:45 +0200

    Cant see the model

--- Engine/Systems/ModelSystem.cs
+++ Engine/Systems/ModelSystem.cs
@@ -10,49 +10,64 @@ namespace Engine.Systems
     public class ModelSystem : IRender, ILoad
     {
         ComponentManager cm = ComponentManager.GetInstance();
+        
 
         public void Load(ContentManager content)
         {
+            Effect effect = content.Load<Effect>("shader");
+
             foreach (var (_, m) in cm.GetComponentsOfType<ModelComponent>())
             {
                 m.Model = content.Load<Model>(m.ModelFile);
-                m.Texture = content.Load<Texture2D>(m.TextureFile);
+                
+                foreach (ModelMesh mesh in m.Model.Meshes)
+                    foreach (BasicEffect currentEffect in mesh.Effects)
+                        m.Texture = currentEffect.Texture;
+
+                foreach (ModelMesh mesh in m.Model.Meshes)
+                    foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                        meshPart.Effect = effect.Clone();
             }
         }
 
+
         public void RenderShadow(GraphicsDevice gd, Effect e)
         {
-            //CameraComponent camera = cm.GetComponentsOfType<CameraComponent>().First().Item2;
+            CameraComponent camera = cm.GetComponentsOfType<CameraComponent>().First().Item2;
 
-            //e.Parameters["LightView"].SetValue(camera.View);
-            //e.Parameters["LightProjection"].SetValue(camera.Projection);
-            //e.Parameters["FogStart"].SetValue(100f);
-            //e.Parameters["FogEnd"].SetValue(1000f);
-            //e.Parameters["FogColor"].SetValue(Color.CornflowerBlue.ToVector3());
-            //e.Parameters["EyePosition"].SetValue(camera.Position);
-            //e.Parameters["LightDirection"].SetValue(new Vector3(-0.5265408f, -0.5735765f, -0.6275069f));
+            e.Parameters["LightView"].SetValue(camera.View);
+            e.Parameters["LightProjection"].SetValue(camera.Projection);
+            e.Parameters["FogStart"].SetValue(100f);
+            e.Parameters["FogEnd"].SetValue(1000f);
+            e.Parameters["FogColor"].SetValue(Color.CornflowerBlue.ToVector3());
+            e.Parameters["EyePosition"].SetValue(camera.Position);
+            e.Parameters["LightDirection"].SetValue(new Vector3(-0.5265408f, -0.5735765f, -0.6275069f));
 
-            //e.Parameters["AmbientColor"].SetValue(Vector3.Zero);
-            //e.Parameters["DiffuseColor"].SetValue(Vector3.One);
-            //e.Parameters["SpecularColor"].SetValue(Vector3.One);
-            //e.Parameters["SpecularPower"].SetValue(120f);
+            e.Parameters["AmbientColor"].SetValue(Vector3.Zero);
+            e.Parameters["DiffuseColor"].SetValue(Vector3.One);
+            e.Parameters["SpecularColor"].SetValue(Vector3.One);
+            e.Parameters["SpecularPower"].SetValue(120f);
 
-            //foreach (var (_, model, trans) in cm.GetComponentsOfType<ModelComponent, TransformComponent>())
-            //{
-            //    foreach (ModelMesh mesh in model.Model.Meshes)
-            //    {
-            //        Matrix[] modelTransforms = new Matrix[model.Model.Bones.Count];
-            //        model.Model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+            foreach (var (_, model, trans) in cm.GetComponentsOfType<ModelComponent, TransformComponent>())
+            {
+                DrawModel(model.Model, model.Texture, trans.World, "Render", e);
+            }
+        }
 
-            //        Matrix worldMatrix = modelTransforms[mesh.ParentBone.Index] * trans.World;
-            //        e.Parameters["Texture"].SetValue(model.Texture);
-            //        e.Parameters["World"].SetValue(worldMatrix);
-            //        e.Techniques["Render"].Passes[0].Apply();
+        private void DrawModel(Model model, Texture2D texture, Matrix wMatrix, string technique, Effect e)
+        {
+            Matrix[] modelTransforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                Matrix worldMatrix = modelTransforms[mesh.ParentBone.Index] * wMatrix;
 
-            //        mesh.Draw();
-            //    }
-                
-            //}
+                e.Parameters["Texture"].SetValue(texture);
+                e.Parameters["World"].SetValue(worldMatrix);
+                e.Techniques[technique].Passes[0].Apply();
+
+                mesh.Draw();
+            }
         }
 
         public void Render(GraphicsDevice gd)
